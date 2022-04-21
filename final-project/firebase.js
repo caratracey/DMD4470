759a0965ccc32845523f6ab742be0d518606cd86
caratracey <60193829+caratracey@users.noreply.github.com>
2022-04-21 11:14:20 -0400

    updating feed

---
+++ final-project/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDVFlNxdtUj_vxHz7V6oIqcUtmFnIPzxD4",
+    authDomain: "final-project-b6509.firebaseapp.com",
+    projectId: "final-project-b6509",
+    storageBucket: "final-project-b6509.appspot.com",
+    messagingSenderId: "295016468688",
+    appId: "1:295016468688:web:2c007879486b1e1cdaced5"
+  };
--- final-project/index.js
+++ final-project/index.js
@@ -3,14 +3,26 @@ new Vue({
     vuetify: new Vuetify(),
     data: function() {
       return {
-        app_title:"Fun Vuetify App",
-        
-      
+        tasks: [
+            {
+                content:"This is my first tweet ever!",
+                username: "caratracey",
+                likes:["joelsalisbury","caratracey", "dan"]
+            },
+            {
+                content:"It's so sunny and nice outside!",
+                username: "caratracey",
+                likes:["joelsalisbury","caratracey", "dan"]
+            },
+
+        ]
       }
-    },
+  },
+
     methods:{
       addTask(){
         console.log('hi')
       }
     }
   })
+
